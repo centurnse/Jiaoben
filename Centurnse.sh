31e3d403cec309a75d10e8146e77924eb1219c19
centurnse <113423652+centurnse@users.noreply.github.com>
2025-02-17 05:35:24 +0800

    Update Centurnse.sh

--- Centurnse.sh
+++ Centurnse.sh
@@ -41,11 +41,12 @@ update_system() {
     info "开始系统更新..."
     case $OS in
         ubuntu|debian)
-            apt update >/dev/null && apt -y upgrade >/dev/null ;;
+            DEBIAN_FRONTEND=noninteractive apt update -qq >/dev/null 
+            DEBIAN_FRONTEND=noninteractive apt -y upgrade -qq >/dev/null ;;
         centos|fedora|rhel)
-            [ "$OS" = "centos" ] && yum -y update >/dev/null ;;
+            yum -y -q update >/dev/null ;;
         alpine)
-            apk update >/dev/null && apk upgrade >/dev/null ;;
+            apk -q update >/dev/null && apk -q upgrade >/dev/null ;;
         *)
             error "不支持的发行版: $OS" ;;
     esac
@@ -60,12 +61,11 @@ install_packages() {
     
     case $OS in
         ubuntu|debian)
-            apt install -y $pkg_list >/dev/null ;;
+            DEBIAN_FRONTEND=noninteractive apt install -y -qq $pkg_list >/dev/null ;;
         centos|fedora|rhel)
-            [ "$OS" = "centos" ] && yum install -y epel-release >/dev/null
-            yum install -y $pkg_list >/dev/null ;;
+            yum install -y -q $pkg_list >/dev/null ;;
         alpine)
-            apk add $pkg_list >/dev/null ;;
+            apk add -q $pkg_list >/dev/null ;;
     esac
     success "组件安装完成"
     progress_bar
@@ -74,7 +74,7 @@ install_packages() {
 # 3. 时区设置
 setup_time() {
     info "设置时区和时间同步..."
-    timedatectl set-timezone Asia/Shanghai || ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+    timedatectl set-timezone Asia/Shanghai 2>/dev/null || ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
     ntpdate pool.ntp.org >/dev/null
     
     # 每小时同步
@@ -195,9 +195,71 @@ setup_ssh() {
         done
     fi
 
-    systemctl restart sshd >/dev/null || systemctl restart ssh >/dev/null
+    systemctl restart sshd 2>/dev/null || systemctl restart ssh 2>/dev/null
     
     success "SSH配置完成"
+    progress_bar
+}
+
+# 8. 网络优化
+setup_network() {
+    info "优化网络配置..."
+    # TCP优化
+    sed -i '/net.ipv4.tcp_no_metrics_save/d' /etc/sysctl.conf
+    sed -i '/net.ipv4.tcp_ecn/d' /etc/sysctl.conf
+    sed -i '/net.ipv4.tcp_frto/d' /etc/sysctl.conf
+    sed -i '/net.ipv4.tcp_mtu_probing/d' /etc/sysctl.conf
+    sed -i '/net.ipv4.tcp_rfc1337/d' /etc/sysctl.conf
+    sed -i '/net.ipv4.tcp_sack/d' /etc/sysctl.conf
+    sed -i '/net.ipv4.tcp_fack/d' /etc/sysctl.conf
+    sed -i '/net.ipv4.tcp_window_scaling/d' /etc/sysctl.conf
+    sed -i '/net.ipv4.tcp_adv_win_scale/d' /etc/sysctl.conf
+    sed -i '/net.ipv4.tcp_moderate_rcvbuf/d' /etc/sysctl.conf
+    sed -i '/net.ipv4.tcp_rmem/d' /etc/sysctl.conf
+    sed -i '/net.ipv4.tcp_wmem/d' /etc/sysctl.conf
+    sed -i '/net.core.rmem_max/d' /etc/sysctl.conf
+    sed -i '/net.core.wmem_max/d' /etc/sysctl.conf
+    sed -i '/net.ipv4.udp_rmem_min/d' /etc/sysctl.conf
+    sed -i '/net.ipv4.udp_wmem_min/d' /etc/sysctl.conf
+    sed -i '/net.core.default_qdisc/d' /etc/sysctl.conf
+    sed -i '/net.ipv4.tcp_congestion_control/d' /etc/sysctl.conf
+
+    cat >> /etc/sysctl.conf << EOF
+net.ipv4.tcp_no_metrics_save=1
+net.ipv4.tcp_ecn=0
+net.ipv4.tcp_frto=0
+net.ipv4.tcp_mtu_probing=0
+net.ipv4.tcp_rfc1337=0
+net.ipv4.tcp_sack=1
+net.ipv4.tcp_fack=1
+net.ipv4.tcp_window_scaling=1
+net.ipv4.tcp_adv_win_scale=1
+net.ipv4.tcp_moderate_rcvbuf=1
+net.core.rmem_max=33554432
+net.core.wmem_max=33554432
+net.ipv4.tcp_rmem=4096 87380 33554432
+net.ipv4.tcp_wmem=4096 16384 33554432
+net.ipv4.udp_rmem_min=8192
+net.ipv4.udp_wmem_min=8192
+net.core.default_qdisc=fq
+net.ipv4.tcp_congestion_control=bbr
+EOF
+
+    # 开启内核转发
+    sed -i '/net.ipv4.conf.all.route_localnet/d' /etc/sysctl.conf
+    sed -i '/net.ipv4.ip_forward/d' /etc/sysctl.conf
+    sed -i '/net.ipv4.conf.all.forwarding/d' /etc/sysctl.conf
+    sed -i '/net.ipv4.conf.default.forwarding/d' /etc/sysctl.conf
+    cat >> '/etc/sysctl.conf' << EOF
+net.ipv4.conf.all.route_localnet=1
+net.ipv4.ip_forward=1
+net.ipv4.conf.all.forwarding=1
+net.ipv4.conf.default.forwarding=1
+EOF
+
+    sysctl -p >/dev/null 2>&1
+    sysctl --system >/dev/null 2>&1
+    success "网络优化完成"
     progress_bar
 }
 
@@ -211,6 +273,7 @@ main() {
     setup_swap
     setup_cleanup
     setup_ssh
+    setup_network
     sep
     success "所有配置已完成！"
 }
